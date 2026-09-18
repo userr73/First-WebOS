b2ff7b99fe30169c595918db4693085e1829a6f7
Grace <wangg2026@abbotsleigh.nsw.edu.au>
2026-09-18 19:18:58 +1000

    Followed a video tutorial to create stopwatch.
    Most of the logic was used (since I've never really coded in js before) but instead of having a start btn, stop btn, and reset btn, I am trying to combine the start and stop btn

--- script.js
+++ script.js
@@ -201,4 +201,51 @@ for (let i = 0; i < content.length; i++) {
 }
 
 
+// Initialise stopwatch app
+initialiseWindow('stopwatch')
+
+let [seconds, minutes, hours] = [0, 0, 0];
+let stopwatchTime = document.getElementById('stopwatchTime');
+let timer = null
+
+function stopwatch() {
+      seconds++;
+
+      if (seconds == 60) {
+            seconds = 0;
+            minutes++;
+
+            if (minutes == 60) {
+                  minutes = 0
+                  hours++;
+            }
+      }
+
+      let h = hours < 10 ? "0" + hours : hours;
+      let m = minutes < 10 ? "0" + minutes : minutes;
+      let s = seconds < 10 ? "0" + seconds : seconds;
+
+      stopwatchTime.innerHTML = h + ':' + m + ':' + s
+}
+
+function startStopwatch() {
+      document.getElementById('stopwatchStartStopBtn').innerHTML = 'Stop'
+
+      if (timer != null) {
+            clearInterval(timer);
+      }
+      
+      timer = setInterval(stopwatch, 1000);
+}
+
+function stopStopwatch() {
+      clearInterval(timer);
+}
+
+function resetStopwatch() {
+      clearInterval(timer);
+      [seconds, minutes, hours] = [0, 0, 0];
+      stopwatchTime.innerHTML = "00:00:00";
+}
+
 
